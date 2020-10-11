2294a9de8f9e469701b8437858abd111207237dc
igihozo-stella <igihozo96@gmail.com>
2020-10-11 15:06:49 +0200

    add variables for input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,8 @@
-//user interface logic
+//business logic
 function myFunction(){
-    var valueSize =document.getElementById('size').value;
-    var valueCrust =document.getElementById('crust').value;
-    alert(parseInt(valueSize)+ parseInt(valueCrust));
+   var valueSize=document.getElementById('size').value;
+   var valueCrust=document.getElementById('size').value;
+   var valueTopping=document.getElementsByClassName('checkbox').value;
+   var numberOfPizza=document.getElementById('numberofpizza').value;
+      alert(parseInt(valueSize)+ parseInt(valueCrust)*parseInt(numberOfPizza));
 }
-/*function getNumberOfPizza() {
-    var selectedNumberOfPizza = document.getElementById("numberofpizza").value;
-    return parseInt(selectedNumberOfPizza);
-}
-function calcTotalAmount() {
-    var totalAmount = (getSizeValue() + getCrust() + getTopping()) * getNumberOfPizza();
-    alert("You have Ordered" + getNumberOfPizza("")  +  " pizza."  +  ""  +  " The Total Amount is "  +  (totalAmount)  +  "rwf"  +  " Thank you for your order welcome again.");
-    prompt("enter your location")
-    alert("You delivery fee is 1500/=")
-    alert("You order will be delivered shortly")
-    alert("Continue shoppping with us each and every time .")
-}*/

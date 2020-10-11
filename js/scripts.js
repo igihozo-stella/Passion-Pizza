1cacf61438b5bdac3d925440820591c5854be3fe
igihozo-stella <igihozo96@gmail.com>
2020-10-11 15:44:03 +0200

    add second function for delivery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 //business logic
 function myFunction(){
-   var valueSize=document.getElementById('size').value;
-   var valueCrust=document.getElementById('size').value;
-   var valueTopping=document.getElementsByClassName('checkbox').value;
-   var numberOfPizza=document.getElementById('numberofpizza').value;
-      alert(parseInt(valueSize)+ parseInt(valueCrust)*parseInt(numberOfPizza));
+   var valueSize =document.getElementById('size').value;
+    var valueCrust=document.getElementById('crust').value;
+    var valueTopping=document.getElementsByClassName('checkbox').value;
+    var numberOfPizza=document.getElementById('numberofpizza').value;
+    var totalAmount=(parseInt(valueSize)+parseInt(valueCrust))*parseInt(numberOfPizza);
+    alert("You have ordered"+" " + numberOfPizza + " pizza." + " " + " The amount to pay is " + totalAmount+ "rwf" + " Thank you for your order!");
+}
+
+function deliver(){
+prompt("enter your location for delivery");
+alert("Your delivery fee is 1500 rwf");
+alert("Your order will be delivered in a short while");
 }
